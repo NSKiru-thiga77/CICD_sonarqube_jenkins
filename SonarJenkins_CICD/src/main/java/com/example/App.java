eca7c9a37eff8a4f0bf9867f81bdc26f32391b8f
iam-nandak <n.kiruthiga9366@gmail.com>
2026-05-03 21:45:15 +0530

    quality gate

--- SonarJenkins_CICD/src/main/java/com/example/App.java
+++ SonarJenkins_CICD/src/main/java/com/example/App.java
@@ -3,5 +3,8 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello EMC, This is KiruthigaNSK, This is a Sonarqube docker Jenkins CI/CD!");
+        System.out.println("Hello EMC, This is KiruthigaNSK, This is a Sonarqube docker Jenkins CI/CD!");
+        System.out.println("Hello EMC, This is KiruthigaNSK, This is a Sonarqube docker Jenkins CI/CD!");
+        System.out.println("Hello EMC, This is KiruthigaNSK, This is a Sonarqube docker Jenkins CI/CD!");
     }
 }
